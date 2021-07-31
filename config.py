e4526358963e28c1fb0cb2953da14529c0229547
parthan12345 <87759173+parthan12345@users.noreply.github.com>
2021-07-31 14:04:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", @MovieHUB_Chat_Group)
 
 #Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
